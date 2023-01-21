94597d8afe44d81ffddcf229906aa2a4ccb417ea
InBok <bbock1214@gmail.com>
2023-01-22 01:56:57 +0900

    문제 일괄 커밋

---
+++ CodeTest_IB/Programmers/Lv1/Q40/Q40_NumberNSpacedByX.java
@@ -0,0 +1,29 @@
+/**
+ * [ 문제 설명 ]
+ * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+ * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+ *
+ * [ 제한 조건 ]
+ * x는 -10000000 이상, 10000000 이하인 정수입니다.
+ * n은 1000 이하인 자연수입니다.
+ *
+ * [ 입출력 예 ]
+ * x	n	answer
+ * 2	5	[2,4,6,8,10]
+ * 4	3	[4,8,12]
+ * -4	2	[-4, -8]
+ */
+
+package Programmers.Lv1.Q40;
+
+public class Q40_NumberNSpacedByX {
+    public static void main(String[] args) {
+        long x = 2;
+        int n = 5;
+        long[] answer = new long[n];
+
+        for(int idx = 0; idx < n; idx++){
+            answer[idx] = x * (idx + 1);
+        }
+    }
+}
