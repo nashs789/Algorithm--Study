4c5b3e6c35d495807f3af88bdf5c62da0fd5771a
InBok <bbock1214@gmail.com>
2023-02-09 23:41:17 +0900

    [ BaekJoon ] 2468번 문제 커밋 - 안전 영역 [ DFS/BFS ]

---
+++ src/BaekJoon/DfsAndBfs/Q2468/Q11720/Q11720_SumOfNumbers.java
@@ -0,0 +1,16 @@
+package BaekJoon.DfsAndBfs.Q2468.Q11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q11720_SumOfNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] input = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.print(Arrays.stream(input).sum());
+    }
+}
