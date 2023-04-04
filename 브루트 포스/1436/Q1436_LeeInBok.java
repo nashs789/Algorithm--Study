0c77e1829089ed655f77d230e4b6e1255c5d8d20
InBok <bbock1214@gmail.com>
2023-04-05 00:17:51 +0900

    [ 4월 첫 번째 주 과제 ] - 분류: 브루트 포스(Brute Force) - 문제:     1. Q2798_블랙잭     2. Q19532_수학은 비대면강의입니다     3. Q1436_영화감독 숌

---
+++ 브루트 포스/1436/Q1436_LeeInBok.java
@@ -0,0 +1,34 @@
+package BaekJoon.BruteForce.Q1436;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Q1436_LeeInBok {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+        int cnt = Integer.parseInt(br.readLine());
+        int curCnt = 0;
+        int start = 666;
+
+        while(true){
+            if(cnt == curCnt){
+                break;
+            }
+
+            if(Integer.toString(start).contains("666")){
+                list.add(start);
+                curCnt++;
+            }
+
+            start++;
+        }
+
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list.get(cnt - 1));
+    }
+}
