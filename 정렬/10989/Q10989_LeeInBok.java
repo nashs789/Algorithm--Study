273dd70baedf35b615986495630bc292e5034185
InBok <bbock1214@gmail.com>
2023-05-01 11:58:38 +0900

    [ 5월 첫 번째 주 과제 ] - 분류: 정렬 (Sorting) - 문제:     1. Q2750_수 정렬하기     2. Q25305_커트라인     3. Q10989_수 정렬하기3     4. Q11650_좌표 정렬하기     5. Q1181_단어 정렬     6. Q18870_좌표 압축
    - 커밋 내용: First Commit

---
+++ 정렬/10989/Q10989_LeeInBok.java
@@ -0,0 +1,36 @@
+package BaekJoon.Sorting.Q10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10989_SortingNumbers3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int cnt = Integer.parseInt(br.readLine());
+        int arr[] = new int[10000001];
+        int max = 0;
+
+        for(int idx = 0; idx < cnt; idx++){
+            int num = Integer.parseInt(br.readLine());
+            arr[num] += 1;
+
+            if(max < num){
+                max = num;
+            }
+        }
+
+        for(int i = 1; i < max + 1; i++){
+            if(arr[i] == 0){
+                continue;
+            }
+
+            for(int j = 0; j < arr[i]; j++){
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ 정렬/2750/Q2750_LeeInBok.java
@@ -0,0 +1,27 @@
+package BaekJoon.Sorting.Q2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q2750_SortNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int idx = 0; idx < N; idx++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        list.sort(Comparator.naturalOrder());
+
+        for(Integer n : list){
+            sb.append(n).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
