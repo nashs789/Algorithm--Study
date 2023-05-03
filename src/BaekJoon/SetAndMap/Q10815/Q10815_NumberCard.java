0a0520d0a7ba25e783ae3e95666006acfc449851
InBok <bbock1214@gmail.com>
2023-05-04 00:00:22 +0900

    [ BaekJoon ] - 문제번호: 10815 - 문제명: 숫자 카드 - 난이도: Silver 5 - 분류: Set And Map

---
+++ src/BaekJoon/SetAndMap/Q10815/Q10815_NumberCard.java
@@ -0,0 +1,37 @@
+package BaekJoon.SetAndMap.Q10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Q10815_NumberCard {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        Set<Integer> has = new HashSet<>();
+
+        for(int idx = 0; idx < N; idx++){
+            has.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int idx = 0; idx < M; idx++){
+            int isHas = Integer.parseInt(st.nextToken());
+
+            if(has.contains(isHas)){
+                sb.append(1).append(' ');
+            } else {
+                sb.append(0).append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
