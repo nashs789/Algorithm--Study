aefed2ff8007d1d79b67b4f496577697c03a9b87
InBok <bbock1214@gmail.com>
2023-05-06 20:35:05 +0900

    [ BaekJoon ] - 문제번호: 24480 - 문제명: 알고리즘 수업 - 깊이 우선 탐색2 - 난이도: Silver 2 - 분류: DFS

---
+++ src/BaekJoon/DfsAndBfs/Q24480/Q24480_AlgorithmClassDFS2.java
@@ -0,0 +1,65 @@
+package BaekJoon.DfsAndBfs.Q24480;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Q24480_AlgorithmClassDFS2 {
+
+    public static int N;
+    public static int M;
+    public static int srt;
+    public static PriorityQueue<Integer>[] adj;
+    public static StringBuilder sb = new StringBuilder();
+    public static boolean[] visited;
+    public static int[] seqArr;
+    public static int seq = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        srt = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        adj = new PriorityQueue[N + 1];
+        seqArr = new int[N + 1];
+
+        for(int idx = 1; idx <= N; idx++){
+            adj[idx] = new PriorityQueue<>(Comparator.reverseOrder());
+        }
+
+        for(int idx = 0; idx < M; idx++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+
+        dfs(srt);
+
+        for(int idx = 1; idx < seqArr.length; idx++){
+            sb.append(seqArr[idx]).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void dfs(int idx){
+        visited[idx] = true;
+        seqArr[idx] = seq++;
+
+        while (!adj[idx].isEmpty()){
+            int linkedNode = adj[idx].poll();
+
+            if(!visited[linkedNode]){
+                dfs(linkedNode);
+            }
+        }
+    }
+}
