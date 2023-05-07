28ade452d427656025ef50d2b9d9cbb5e24f1cec
hhp227study <83169005+hhp227study@users.noreply.github.com>
2023-05-07 18:47:58 +0900

    Create Q1427_HuipyoHong.swift

---
+++ 정렬/1427/Q1427_HuipyoHong.swift
@@ -0,0 +1,13 @@
+//
+//  Q1427_HuipyoHong.swift
+//
+//
+//  Created by 홍희표 on 2023/05/07.
+//
+/**
+ *  [ 효율성 ]
+ *  - 메모리: 69104KB
+ *  - 시간 : 8ms
+ */
+
+print(readLine()!.map { Int(String($0))! }.sorted(by: >).map { String($0) }.joined())
