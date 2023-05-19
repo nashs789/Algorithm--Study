404fd73560b5faa01c24b88e58e2cebec9081a04
KIM EUNBI <65601654+eunbi99@users.noreply.github.com>
2023-05-19 22:53:21 +0900

    Create Q1269_Eunbi.java
    집합과 맵 - 1269

---
+++ 집합과 맵/Q1269/Q1269_Eunbi.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+/**
+  백준  - 집합과 맵  
+  1269. 대칭 차집합
+  
+  메모리 : 83256KB
+  시간  : 724ms
+**/
+public class Q1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        Set<Integer> result = new HashSet<>();
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i= 0; i<A; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            result.add(num);
+        }
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i= 0; i<B; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(result.contains(num)) {
+                result.remove(num);
+            } else {
+                result.add(num);
+            };
+        }
+        System.out.println(result.size());
+        br.close();
+    }
+}
