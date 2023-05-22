bcafa0910ec7e78557dbb264fd0429ba0f4c574a
InBok <bbock1214@gmail.com>
2023-05-23 00:51:57 +0900

    [ BaekJoon ] - 문제번호: 2580 - 문제명: 수도쿠 - 난이도: Gold 4 - 분류: BackTracking
    커밋 내용: 주석 추가

--- src/BaekJoon/BackTracking/Q2580/Q2580_Sudoku.java
+++ src/BaekJoon/BackTracking/Q2580/Q2580_Sudoku.java
@@ -12,9 +12,9 @@ public class Q2580_Sudoku {
 
     public static int[][] sudoku;
     // visited 는 방문 보다는 숫자가 있음 유무
-    public static boolean[][] visitedRow;
-    public static boolean[][] visitedCol;
-    public static boolean[][] visitedArea;
+    public static boolean[][] visitedRow;   // 가로의 숫자(1 ~ 9) 체크
+    public static boolean[][] visitedCol;   // 세로의 숫자(1 ~ 9) 체크
+    public static boolean[][] visitedArea;  // 3 * 3 사이즈 숫자(1 ~ 9) 체크
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -33,7 +33,7 @@ public class Q2580_Sudoku {
                 if(row[j] != 0){
                     visitedRow[i][row[j] - 1] = true;
                     visitedCol[j][row[j] - 1] = true;
-                    visitedArea[(i / 3) * 3 + (j / 3)][row[j] - 1] = true;
+                    visitedArea[getSquareIdx(i, j)][row[j] - 1] = true;
                 }
             }
         }
@@ -44,16 +44,16 @@ public class Q2580_Sudoku {
         for(int i = x; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 if(sudoku[i][j] == 0){
-                    Iterator<Integer> it = getPossibilities(i, j).iterator();
+                    Iterator<Integer> it = getPossibilities(i, j).iterator();   // 현재 위치에 들어갈 수 있는 숫자들 얻어옴
 
                     while(it.hasNext()){
                         int num = it.next() - 1;
 
                         if(isOk(i, j, num)){
-                            sudoku[i][j] = num + 1;
-                            visitedRow[i][num] = true;
-                            visitedCol[j][num] = true;
-                            visitedArea[getSquareIdx(i, j)][num] = true;
+                            sudoku[i][j] = num + 1;                      // 빈 칸에 숫자 입력
+                            visitedRow[i][num] = true;                   // 해당 칸에 해당하는 row 에 숫자 채움
+                            visitedCol[j][num] = true;                   // 해당 칸에 해당하는 col 에 숫자 채움
+                            visitedArea[getSquareIdx(i, j)][num] = true; // 해당 칸에 해당하는 3 * 3 정사각형에 에 숫자 채움
                             backTracking(i);
                             sudoku[i][j] = 0;
                             visitedRow[i][num] = false;
@@ -61,11 +61,12 @@ public class Q2580_Sudoku {
                             visitedArea[getSquareIdx(i, j)][num] = false;
                         }
                     } // while end
-                    return;
+                    return;    // 모든 숫자 순회 후 빈칸을 충족하는 숫자가 없는 경우 리턴
                 }
             }
         }
 
+        // 반복문을 통해 [8][8] 까지 전부 채운 경우 출력 후 종료
         for(int a[] : sudoku){
             for(int b : a){
                 sb.append(b).append(' ');
@@ -76,6 +77,12 @@ public class Q2580_Sudoku {
         System.exit(0);
     }
 
+    /**
+     * 빈칸에 들어갈 수 있는 모든 숫자 반환
+     * @param x: 좌표 x
+     * @param y: 좌표 y
+     * @return
+     */
     static Set<Integer> getPossibilities(int x, int y){
         Set<Integer> set = new HashSet<>();
 
@@ -98,6 +105,13 @@ public class Q2580_Sudoku {
         return set;
     }
 
+    /**
+     * 현재 빈 칸에 해당하는 숫자가 들어갈 수 있는지 체크
+     * @param x: 좌표 x
+     * @param y: 좌표 y
+     * @param num: 숫자 (1 ~ 9)
+     * @return
+     */
     static boolean isOk(int x, int y, int num){
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++) {
@@ -109,6 +123,7 @@ public class Q2580_Sudoku {
         return true;
     }
 
+    // 3 * 3 정사각형 사이즈 인덱스 구하는 메소드
     static int getSquareIdx(int x, int y) {
         return (x / 3) * 3 + (y / 3);
     }
