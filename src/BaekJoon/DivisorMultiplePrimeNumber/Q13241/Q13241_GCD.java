4531f494c2397a3cd9ce0d58a6833ea9d41f2bc7
InBok <bbock1214@gmail.com>
2023-06-06 00:22:07 +0900

    [ BaekJoon ] - 문제번호: 13241 - 문제명: GCD - 난이도: Silver 5 - 분류: Math

---
+++ src/BaekJoon/DivisorMultiplePrimeNumber/Q13241/Q13241_GCD.java
@@ -0,0 +1,20 @@
+package BaekJoon.DivisorMultiplePrimeNumber.Q13241;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Q13241_GCD {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+
+        long num1 = Integer.parseInt(arr[0]);
+        long num2 = Integer.parseInt(arr[1]);
+        BigInteger bigNum = new BigInteger(arr[0]);
+        int gcd = bigNum.gcd(new BigInteger(arr[1])).intValue();
+
+        System.out.println(num1 * num2 / gcd);
+    }
+}
