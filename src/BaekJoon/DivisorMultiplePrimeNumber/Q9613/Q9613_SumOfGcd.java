1bbc3713ee5a767f4dc3e564c35d37687dbee819
InBok <bbock1214@gmail.com>
2023-06-05 20:03:52 +0900

    [ BaekJoon ] - 문제번호: 9613 - 문제명: GCD의 합 - 난이도: Silver 5 - 분류: Math

---
+++ src/BaekJoon/DivisorMultiplePrimeNumber/Q9613/Q9613_SumOfGcd.java
@@ -0,0 +1,30 @@
+package BaekJoon.DivisorMultiplePrimeNumber.Q9613;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Q9613_SumOfGcd {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int testCnt = Integer.parseInt(br.readLine());
+
+        for(int idx = 0; idx < testCnt; idx++){
+            String[] arr = br.readLine().split(" ");
+
+            long sum = 0;
+
+            for(int i = 1; i < arr.length - 1; i++){
+                for(int j = i + 1; j < arr.length; j++){
+                    BigInteger a = new BigInteger(arr[i]);
+                    sum += a.gcd(new BigInteger(arr[j])).intValue();
+                }
+            }
+            sb.append(sum).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
