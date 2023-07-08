554bfb0b49dd4790a126868eb953442830a5d234
InBok <bbock1214@gmail.com>
2023-07-08 21:41:38 +0900

    [ 스터디 과제 ] - 주차: 7월 2주차 - 분류: 큐 - 문제:     - Q5430_AC
    - Comment: 주석 추가

--- SecondRound(2023.07)/2주차_큐, 데크/Q5430/Q5430_LeeInBok.java
+++ SecondRound(2023.07)/2주차_큐, 데크/Q5430/Q5430_LeeInBok.java
@@ -24,11 +24,16 @@ public class Q5430_AC {
         for(int idx = 0; idx < TC; idx++){
             char[] regs = br.readLine().toCharArray();
             int numCnt = Integer.parseInt(br.readLine());
+            // 1. '[', ']' 제거 ===> replace 보다는 첫 글자랑 마지막 글자 지우는게 효율면에서 좋음
+            // 2. ',' 기준으로 숫자들 잘라서 배열 요소로 만든다.
+            // 3. 공백인 경우는 제외한다. (공백은 캐스팅 불가능 ===> 에러 발생)
+            // 4. Wrapper Class 로 Boxing
+            // 5. List 로 변환하는 이유는 addAll 이 콜렉션 객체만 파라미터로 받기 떄문에
             List<Integer> seq = Arrays.stream(br.readLine().replaceAll("[\\[\\]]", "").split(","))
-                                      .filter(e -> !e.equals(""))
-                                      .mapToInt(Integer::parseInt)
-                                      .boxed()
-                                      .collect(Collectors.toList());
+                    .filter(e -> !e.equals(""))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
             LinkedList<Integer> deque = new LinkedList<>();
             boolean isReverse = false;
             boolean error = false;
@@ -40,7 +45,9 @@ public class Q5430_AC {
                 if(r == 'R'){
                     isReverse = !isReverse;
                 } else {
+                    // 큐가 비어있는지 체크 후 false 케이스는 error
                     if(!deque.isEmpty()){
+                        // isReverse: ture ===> 역방향
                         if(isReverse){
                             deque.pollLast();
                         } else {
@@ -54,15 +61,20 @@ public class Q5430_AC {
                 }
             }
 
+            // 에러인 경우 요소를 출력할 필요가 없음(이미 위에서 error로 입력함)
             if(!error){
                 Iterator<Integer> it;
 
+                // 정방향, 역방향 확인 후 iterator 출력 방향 결정
                 if(isReverse){
                     it = deque.descendingIterator();
                 } else {
                     it = deque.iterator();
                 }
 
+                // 출력 포맷에 맞춰서 파싱
+                // 1. 큐가 비어있을 떄: 남은 배열 요소가 없음
+                // 2. 큐가 비어있지 않음: 남은 배열 요소 출력 및 파싱
                 if(deque.isEmpty()){
                     sb.append('[').append(']').append('\n');
                 } else {
