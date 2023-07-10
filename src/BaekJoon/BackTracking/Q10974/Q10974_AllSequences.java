918f9f0b3979abc03a0857e94fad9e24a21dc68f
InBok <bbock1214@gmail.com>
2023-07-10 22:49:59 +0900

    [ BaekJoon ] - 문제번호: 2606, 10974 - 문제명: 바이러스, - 난이도: Silver 2, Silver 3 - 분류: BackTracking

---
+++ src/BaekJoon/BackTracking/Q10974/Q10974_AllSequences.java
@@ -0,0 +1,43 @@
+package BaekJoon.BackTracking.Q10974;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10974_AllSequences {
+
+    public static StringBuilder sb = new StringBuilder();
+    public static boolean[] visited;
+    public static int[] seq;
+    public static int N;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N + 1];
+        seq = new int[N];
+
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int len){
+        if(len == N){
+            for(int n : seq){
+                sb.append(n + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int idx = 1; idx <= N; idx++){
+            if(!visited[idx]){
+                visited[idx] = true;
+                seq[len] = idx;
+                backTracking(len + 1);
+                visited[idx] = false;
+            }
+        }
+    }
+}
