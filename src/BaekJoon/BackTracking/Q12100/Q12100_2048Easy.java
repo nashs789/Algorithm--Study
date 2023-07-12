521b33c46123a0b078a8fe09ccb1db4cc0eb7f38
InBok <bbock1214@gmail.com>
2023-07-12 14:21:31 +0900

    [ BaekJoon ] - 문제번호: 12100 - 문제명: 2048(Easy) - 난이도: Gold 2 - 분류: BackTracking

--- src/BaekJoon/BackTracking/Q12100/Q12100_2048Easy.java
+++ src/BaekJoon/BackTracking/Q12100/Q12100_2048Easy.java
@@ -7,8 +7,8 @@ import java.util.StringTokenizer;
 
 public class Q12100_2048Easy {
 
-    public static int N;
-    public static int max = Integer.MIN_VALUE;
+    public static int N;                          // N * N 사이즈 board
+    public static int max = Integer.MIN_VALUE;    // 현재 board 에서 최대 값
     public static int[][] move = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) throws IOException {
@@ -30,12 +30,14 @@ public class Q12100_2048Easy {
     }
 
     public static void backTracking(int round, int[][] board){
+        // 최대 이동 횟수 5번
         if(round == 5){
             max = Math.max(max, getMaxValue(board));
             return;
         }
 
         for(int i = round; i < 5; i++){
+            // 4방향 이동
             for(int j = 0; j < 4; j++) {
                 backTracking(i + 1, moveBoard(j, board));
             }
@@ -45,7 +47,7 @@ public class Q12100_2048Easy {
     // 0: 상, 1: 하, 2: 좌, 3: 우
     public static int[][] moveBoard(int direction, int[][] board){
         int[][] newBoard = copy(board);
-        boolean[][] sum = new boolean[N][N];
+        boolean[][] sum = new boolean[N][N];    // 이미 합쳐진 경우 처리 배열
 
         switch (direction){
             case 0:
@@ -99,20 +101,22 @@ public class Q12100_2048Easy {
     }
 
     public static void move(int i, int j, int direction, int[][] board, boolean[][] sum){
+        // nexX, newY: 현재 위치에서 이동 or 합쳐질 좌표
         int newX = i + move[direction][0];
         int newY = j + move[direction][1];
-        int newI = i;
-        int newJ = j;
 
         if(isInvalidCoord(newX, newY)){
             return;
         }
 
+        // 이동할 위치가 0인 케이스
+        // 0이 더 이상 나오지 않거나 유효하지 않은 좌표가 나올 때 까지 이동한다
+        // 이동시: 현재 좌표와 다음 좌표가 direction 만큼 이동한다
         while(!isInvalidCoord(newX, newY) && board[newX][newY] == 0){
-            board[newX][newY] = board[newI][newJ];
-            board[newI][newJ] = 0;
-            newI = newX;
-            newJ = newY;
+            board[newX][newY] = board[i][j];
+            board[i][j] = 0;
+            i = newX;
+            j = newY;
             newX += move[direction][0];
             newY += move[direction][1];
         }
@@ -121,13 +125,16 @@ public class Q12100_2048Easy {
             return;
         }
 
-        if(board[newX][newY] != 0 && board[newX][newY] == board[newI][newJ] && !sum[newX][newY]){
-            board[newX][newY] += board[newI][newJ];
+        // 현재 좌표와 이동할 좌표가 갖는 수가 같을 경우 값을 합해준다.
+        // 이동할 좌표 값이 0이 아닌경우 && 이동할 보드 좌표의 값 == 현재 보드 좌표의 값 && 합해진 적 없는 케이스
+        if(board[newX][newY] != 0 && board[newX][newY] == board[i][j] && !sum[newX][newY]){
+            board[newX][newY] += board[i][j];
             sum[newX][newY] = true;
-            board[newI][newJ] = 0;
+            board[i][j] = 0;    // 값이 합해진 후 현재 좌표 값은 0으로 변경
         }
     }
 
+    // board 주소 값을 공유할 수 없으니 새로 복사해서 사용하게 해주는 함수 (2차원 배열 복사)
     public static int[][] copy(int[][] board){
         int[][] copyBoard = new int[N][];
 
@@ -138,10 +145,12 @@ public class Q12100_2048Easy {
         return copyBoard;
     }
 
+    // 좌표가 유효하지 않음: true, 유효함: false
     public static boolean isInvalidCoord(int x, int y){
         return x < 0 || x >= N || y < 0 || y >= N;
     }
 
+    // board 에서 가장 큰 값을 찾음
     public static int getMaxValue(int[][] board){
         int localMax = Integer.MIN_VALUE;
 
@@ -154,11 +163,3 @@ public class Q12100_2048Easy {
         return localMax;
     }
 }
-
-// 0인경우: 비어있을 경우 이동
-// 0이 아닌데 같은 숫자인경우: 숫자가 합쳐지고 현 위치 0으로
-// 0이 아닌데 다른 숫자인경우: 움직임 없음
-
-// 9
-// 11:32 - 13:30
-// 2시간 11분
