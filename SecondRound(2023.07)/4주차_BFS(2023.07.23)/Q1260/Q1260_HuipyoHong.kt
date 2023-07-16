02f982b9cde257cf3530daa6929da44888b0973e
hhp227study <83169005+hhp227study@users.noreply.github.com>
2023-07-17 02:01:17 +0900

    Create Q1260_HuipyoHong.kt

---
+++ SecondRound(2023.07)/4주차_BFS(2023.07.23)/Q1260/Q1260_HuipyoHong.kt
@@ -0,0 +1,46 @@
+/**
+ *  [ 효율성 ]
+ *  - 메모리: 33972KB
+ *  - 시간 : 436ms
+ */
+
+import java.util.*
+
+fun main() {
+    val (n, m, v) = readln().split(" ").map(String::toInt)
+    val map = (1..n).zip(Array(n) { mutableListOf<Int>() }).toMap().apply {
+        repeat(m) {
+            readln().split(" ").map(String::toInt).also { (a, b) ->
+                get(a)?.add(b)
+                get(b)?.add(a)
+            }
+        }
+        for (list in values) list.sort()
+    }
+    val dfsVisited = mutableSetOf(v)
+    val bfsVisited = mutableSetOf(v)
+    fun dfs(start: Int) {
+        for (next in map.getOrDefault(start, mutableListOf())) {
+            if (dfsVisited.contains(next)) continue
+            dfsVisited.add(next)
+            dfs(next)
+        }
+    }
+    fun bfs(start: Int) {
+        val queue = LinkedList(arrayListOf(start))
+
+        while (queue.isNotEmpty()) {
+            val outed = queue.poll()
+
+            bfsVisited.add(outed)
+            for (next in map.getOrDefault(outed, mutableListOf())) {
+                if (!bfsVisited.contains(next) && !queue.contains(next)) queue.offer(next)
+            }
+        }
+    }
+
+    dfs(v)
+    bfs(v)
+    println(dfsVisited.joinToString(" "))
+    println(bfsVisited.joinToString(" "))
+}
